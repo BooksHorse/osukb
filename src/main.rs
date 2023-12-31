f6fa9a30209a860f72ce797d31535fc732d1fb2b
mochi <bookshorse@outlook.jp>
2024-01-01 04:52:02 +0700

    multicore

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,27 @@
 
 const N_SAMPLES: i32 = 10;
 const TIMEOUT_TICKS: u16 = 10000;
+extern crate alloc;
+use alloc::rc::{self, Rc};
+use core::cell::{Ref, RefCell};
+use cortex_m::interrupt::{self, Mutex};
+use embedded_alloc::Heap;
+use embedded_graphics::{
+    mono_font::{ascii::FONT_9X18_BOLD, MonoTextStyleBuilder},
+    pixelcolor::BinaryColor,
+    prelude::*,
+    text::{Baseline, Text},
+};
+use ssd1306::{prelude::*, Ssd1306};
+#[global_allocator]
+static HEAP: Heap = Heap::empty();
 
+use core::borrow::BorrowMut;
+use cortex_m::delay::Delay;
+use embedded_hal::digital::v2::{InputPin, OutputPin};
 use embedded_hal::timer::CountDown;
-use fugit::ExtU32;
+use fugit::{ExtU32, RateExtU32};
+use hal::Timer;
 use usbd_human_interface_device::descriptor::InterfaceProtocol;
 use usbd_human_interface_device::device::keyboard::{
     BootKeyboardConfig, BOOT_KEYBOARD_REPORT_DESCRIPTOR,
@@ -15,12 +33,8 @@ use usbd_human_interface_device::page::Keyboard;
 use usbd_human_interface_device::prelude::*;
 use vcc_gnd_yd_rp2040::hal::multicore::{Multicore, Stack};
 
-use core::borrow::BorrowMut;
-use cortex_m::delay::Delay;
-use embedded_hal::digital::v2::{InputPin, OutputPin};
-use hal::Timer;
-
-use panic_halt as _;
+// use panic_halt as _;
+use panic_probe as _;
 // use rp2040_hal as halpi;
 use usb_device::{class_prelude::*, prelude::*};
 
@@ -35,9 +49,14 @@ use vcc_gnd_yd_rp2040::{
     },
     Pins, XOSC_CRYSTAL_FREQ,
 };
-//static mut CORE1_STACK: Stack<4096> = Stack::new();
+static mut CORE1_STACK: Stack<4096> = Stack::new();
+
+static TIMER: Mutex<RefCell<Option<Timer>>> = Mutex::new(RefCell::new(None));
+use rtt_target::{rprintln, rtt_init_print};
 #[entry]
 fn main() -> ! {
+    rtt_init_print!();
+    rprintln!("Hello, world!");
     let mut pac = pac::Peripherals::take().unwrap();
     let core = pac::CorePeripherals::take().unwrap();
 
@@ -57,6 +76,7 @@ fn main() -> ! {
 
     let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
     let timer = Timer::new(pac.TIMER, &mut pac.RESETS);
+    interrupt::free(|cs| TIMER.borrow(cs).replace(Some(timer)));
     let mut sio = Sio::new(pac.SIO);
     let pins = Pins::new(
         pac.IO_BANK0,
@@ -122,94 +142,167 @@ fn main() -> ! {
     // let mut input_count_down = timer.count_down();
     // input_count_down.start(1.millis());
 
-    let mut tick_count_down = timer.count_down();
-    tick_count_down.start(1.millis());
-
     // let mut tick_osuclick = timer.count_down();
     // tick_osuclick.start(1.millis());
     let mut keys: [Keyboard; 2] = [Keyboard::NoEventIndicated; 2];
 
-    // let mut mc = Multicore::new(&mut pac.PSM, &mut pac.PPB, &mut sio.fifo);
-    // let cores = mc.cores();
-    // let core1 = &mut cores[1];
-    // let _test = core1.spawn(unsafe { &mut CORE1_STACK.mem }, core1_task);
-    loop {
-        // if tick_osuclick.wait().is_ok() {
-        //  critical_section::with(|_| {
-        // push_kb_movement(reportA).ok().unwrap_or(0);
-        let value0 = a.value(&mut delay);
+    let mut mc = Multicore::new(&mut pac.PSM, &mut pac.PPB, &mut sio.fifo);
+    let cores = mc.cores();
+    let core1 = &mut cores[1];
 
-        if value0 {
-            //button_pin.is_low().unwrap()
-            led_pin.set_high().unwrap();
-            //let _ = serial.write(b"A\n");
-            keys[0] = Keyboard::A;
-            //push_kb_movement(reportA).ok().unwrap_or(0);
-        } else {
-            led_pin.set_low().unwrap();
-            keys[0] = Keyboard::NoEventIndicated;
-        }
-        let value1 = b.value(&mut delay);
-        if value1 {
-            led_pin.set_high().unwrap();
-            //let _ = serial.write(b"A\n");
-            keys[1] = Keyboard::D;
-            //push_kb_movement(reportA).ok().unwrap_or(0);
-        } else {
-            led_pin.set_low().unwrap();
-            keys[1] = Keyboard::NoEventIndicated;
-        }
-        //     //let _ = serial.write(b"D\n");
-        //     //push_kb_movement(reportD).ok().unwrap_or(0);
-        // } else {
-        //     led_pin.set_low().unwrap();
-        // }
-        // usb_dev.poll(&mut [&mut serial]);
-        //     }
-        //)
-        //}
+    //let mut sda_pin = pins.gpio18.into_mode();
+    //let mut scl_pin = pins.gpio19.into_mode();
+    let mut test_pin = pins.gpio0.into_push_pull_output();
+    let mut cc = pac.I2C1;
 
-        // if input_count_down.wait().is_ok() {
-        //let keys = Keyboard::NoEventIndicated;
+    let _test = core1
+        .spawn(unsafe { &mut CORE1_STACK.mem }, move || -> ! {
+            interrupt::free(|cs| {
+                test_pin.set_high().unwrap();
+                // let i2c = hal::I2C::i2c1(
+                //     cc,
+                //     sda_pin,
+                //     scl_pin,
+                //     RateExtU32::kHz(4000),
+                //     &mut pac.RESETS,
+                //     &clocks.peripheral_clock,
+                // );
 
-        match keyboard.device().write_report(keys) {
-            Err(UsbHidError::WouldBlock) => {}
-            Err(UsbHidError::Duplicate) => {}
-            Ok(_) => {}
-            Err(e) => {
-                core::panic!("Failed to write keyboard report: {:?}", e)
+                // // // Create the I²C display interface:
+                // let interface = ssd1306::I2CDisplayInterface::new(i2c);
+
+                // let mut display =
+                //     Ssd1306::new(interface, DisplaySize128x64, DisplayRotation::Rotate0)
+                //         .into_buffered_graphics_mode();
+                // display.init().unwrap();
+
+                // // // Create a text style for drawing the font:
+                // let text_style = MonoTextStyleBuilder::new()
+                //     .font(&FONT_9X18_BOLD)
+                //     .text_color(BinaryColor::On)
+                //     .build();
+                let mut a = false;
+                loop {
+                    let tm = TIMER.borrow(cs).take();
+                    if tm.is_some() {
+                        let tr = tm.ok_or("a");
+                        let td = match tr {
+                            Ok(e) => e,
+                            Err(a) => continue,
+                        };
+                        let mut tick_count_down = td.count_down();
+                        tick_count_down.start(1000.millis());
+
+                        if tick_count_down.wait().is_ok() {
+                            //     let _ = display.clear(BinaryColor::On);
+                            //     Text::with_baseline(
+                            //         "Hello world!",
+                            //         Point::zero(),
+                            //         text_style,
+                            //         Baseline::Top,
+                            //     )
+                            //     .draw(&mut display)
+                            //     .unwrap();
+                            //     // if a {
+                            //     //     test_pin.set_high().unwrap();
+                            //     // } else {
+                            //     //     test_pin.set_low().unwrap();
+                            //     // }
+                            a = !a;
+                            //     display.flush().unwrap();
+                        }
+                    }
+                }
+            })
+        })
+        .unwrap();
+
+    interrupt::free(|cs| {
+        let tm = TIMER.borrow(cs).take();
+        let td = tm.unwrap();
+        let mut tick_count_down = td.count_down();
+        tick_count_down.start(1.millis());
+
+        loop {
+            // if tick_osuclick.wait().is_ok() {
+            //  critical_section::with(|_| {
+            // push_kb_movement(reportA).ok().unwrap_or(0);
+            let value0 = a.value(&mut delay);
+
+            if value0 {
+                //button_pin.is_low().unwrap()
+                led_pin.set_high().unwrap();
+                //let _ = serial.write(b"A\n");
+                keys[0] = Keyboard::A;
+                //push_kb_movement(reportA).ok().unwrap_or(0);
+            } else {
+                led_pin.set_low().unwrap();
+                keys[0] = Keyboard::NoEventIndicated;
             }
-        };
-        // }
-        if tick_count_down.wait().is_ok() {
-            match keyboard.tick() {
+            let value1 = b.value(&mut delay);
+            if value1 {
+                led_pin.set_high().unwrap();
+                //let _ = serial.write(b"A\n");
+                keys[1] = Keyboard::D;
+                //push_kb_movement(reportA).ok().unwrap_or(0);
+            } else {
+                led_pin.set_low().unwrap();
+                keys[1] = Keyboard::NoEventIndicated;
+            }
+            //     //let _ = serial.write(b"D\n");
+            //     //push_kb_movement(reportD).ok().unwrap_or(0);
+            // } else {
+            //     led_pin.set_low().unwrap();
+            // }
+            // usb_dev.poll(&mut [&mut serial]);
+            //     }
+            //)
+            //}
+
+            // if input_count_down.wait().is_ok() {
+            //let keys = Keyboard::NoEventIndicated;
+
+            match keyboard.device().write_report(keys) {
                 Err(UsbHidError::WouldBlock) => {}
+                Err(UsbHidError::Duplicate) => {}
                 Ok(_) => {}
                 Err(e) => {
-                    core::panic!("Failed to process keyboard tick: {:?}", e)
+                    core::panic!("Failed to write keyboard report: {:?}", e)
                 }
             };
-        }
-
-        if usb_dev.poll(&mut [&mut keyboard]) {
-            match keyboard.device().read_report() {
-                Err(UsbError::WouldBlock) => {
-                    //do nothing
-                }
-                Err(e) => {
-                    core::panic!("Failed to read keyboard report: {:?}", e)
-                }
-                Ok(_leds) => {}
+            // }
+            if tick_count_down.wait().is_ok() {
+                match keyboard.tick() {
+                    Err(UsbHidError::WouldBlock) => {}
+                    Ok(_) => {}
+                    Err(e) => {
+                        core::panic!("Failed to process keyboard tick: {:?}", e)
+                    }
+                };
             }
+
+            if usb_dev.poll(&mut [&mut keyboard]) {
+                match keyboard.device().read_report() {
+                    Err(UsbError::WouldBlock) => {
+                        //do nothing
+                    }
+                    Err(e) => {
+                        core::panic!("Failed to read keyboard report: {:?}", e)
+                    }
+                    Ok(_leds) => {}
+                }
+            }
+            // if button_pin.is_high().unwrap() {
+            //     led_pin.set_high().unwrap();
+            //     //let _ = serial.write(b"A\n");
+            //     //push_kb_movement(reportA).ok().unwrap_or(0);
+            // } else {
+            //     led_pin.set_low().unwrap();
+            // }
         }
-        // if button_pin.is_high().unwrap() {
-        //     led_pin.set_high().unwrap();
-        //     //let _ = serial.write(b"A\n");
-        //     //push_kb_movement(reportA).ok().unwrap_or(0);
-        // } else {
-        //     led_pin.set_low().unwrap();
-        // }
-    }
+    });
+
+    loop {}
 }
 
 // fn core1_task() -> ! {
